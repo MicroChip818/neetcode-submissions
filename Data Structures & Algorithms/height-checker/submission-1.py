7711149e33a35e9f7052bbf712a818b9cc5a2065
Ethan Khoo <ekhoock10@gmail.com>
2026-08-05 08:45:19 -0700

    Add: height-checker - submission-1

---
+++ Data Structures & Algorithms/height-checker/submission-1.py
@@ -0,0 +1,18 @@
+import heapq
+from copy import copy
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        copy_heights = copy(heights)
+        heapq.heapify(copy_heights)
+        sorted_heights = []
+        
+        while copy_heights:
+            sorted_heights.append(heapq.heappop(copy_heights))
+        print(sorted_heights)
+        res = 0
+        for i, height in enumerate(heights):
+            if height != sorted_heights[i]:
+                res += 1
+        
+        return res
